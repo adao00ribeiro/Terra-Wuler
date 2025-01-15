dbb908c4255208733308ec2172f09e74aa1f0726
Rian Brunelli <84146275+rianbr10@users.noreply.github.com>
2025-01-15 10:48:30 -0300

    TW-14: Arquivo DataCharacter para instanciar infos do personagem.

---
+++ Assets/Scripts/Scriptables/DataCharacter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DataCharacter", menuName = "Data/Character")]
+public class CharacterData : ScriptableObject
+{
+    [SerializeField] private string _name;
+    [SerializeField] private Sprite _avatar;
+    [SerializeField] private GameObject _prefabCharacter;
+
+    public string Name { get => _name; }
+    public Sprite Avatar { get => _avatar; }
+    public GameObject PrefabCharacter { get => _prefabCharacter; }
+}
