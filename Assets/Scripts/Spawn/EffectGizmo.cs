91e81b70819e80db504f654ccba564bc47c93e6a
Murilo Sartori <89427588+MuriloSart@users.noreply.github.com>
2025-01-20 17:46:11 -0300

    feat EffectGizmo

---
+++ Assets/Scripts/Spawn/EffectGizmo.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public enum GizmoType
+{
+    Cube,
+    Sphere
+}
+
+public class EffectGizmo : MonoBehaviour
+{
+    [Header("Gizmo Settings")]
+    public Color32 gizmoColor = Color.white;
+    public GizmoType gizmoType = GizmoType.Cube;
+    public float radius = 1f;
+    public Vector3 size = Vector3.one;
+
+    private Vector3 pivotOffset = Vector3.zero;
+
+    public virtual void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+
+        switch (gizmoType)
+        {
+            case GizmoType.Cube:
+                pivotOffset.y = -size.y / 2;
+                Gizmos.DrawWireCube(transform.position - pivotOffset, size);
+                break;
+
+            case GizmoType.Sphere:
+                Gizmos.DrawWireSphere(transform.position - pivotOffset, radius);
+                break;
+
+            default:
+                Debug.LogWarning($"GizmoType '{gizmoType}' não é suportado.");
+                break;
+        }
+    }
+
+    private void OnValidate()
+    {
+        radius = Mathf.Max(0, radius);
+        size = new Vector3(
+            Mathf.Max(0, size.x),
+            Mathf.Max(0, size.y),
+            Mathf.Max(0, size.z)
+        );
+    }
+}
