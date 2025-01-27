cf97f97d8589c0ceca95ebb3708699d3fae34029
Adão Ribeiro <adao-eduardo@hotmail.com>
2025-01-27 08:11:08 -0300

    Prototipo Arqueiro

--- Assets/Scripts/SpawnerEnemyManager.cs
+++ Assets/Scripts/SpawnerEnemyManager.cs
@@ -11,7 +11,10 @@ namespace TerraWuler
         public int maxActiveEnemies = 10; 
         private int currentEnemyCount = 0;
 
-
+        void Start(){
+           DataManager DATA =  GameController.Instance.GetComponentManager<DataManager>();
+           InputManager input =  GameController.Instance.GetComponentManager<InputManager>();
+        }
         public bool CanSpawn()
         {
             return currentEnemyCount < maxActiveEnemies;
