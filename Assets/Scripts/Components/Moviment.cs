83dce7acd0f744066a1299e35db61977ecfe13a6
Rian Brunelli <84146275+rianbr10@users.noreply.github.com>
2025-01-28 15:48:49 -0300

    TW-34: Criado a movimentação do Rigidbody, permitindo andar e pular.

--- Assets/Scripts/Components/Moviment.cs
+++ Assets/Scripts/Components/Moviment.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class Moviment : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Rigidbody playerRigidbody;
+    private float speed = 700f;
+    private float jumpForce = 10f;
+    private Vector3 movement;
+    private bool isGrounded;
+
+    private void Update()
     {
-        
+        InputMovement();
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            StartCoroutine(SmoothJump());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        PhysicsMoviment();
+
+        //Suavizar a queda do pulo
+        if (!isGrounded)
+        {
+            playerRigidbody.AddForce(Vector3.down * 20f, ForceMode.Acceleration);
+        }
+    }
+
+    private void InputMovement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        movement = (transform.right * moveHorizontal) + (transform.forward * moveVertical);
+    }
+
+    private void PhysicsMoviment()
+    {
+        playerRigidbody.AddForce(movement.normalized * speed * Time.fixedDeltaTime);
+    }
+
+    private IEnumerator SmoothJump()
+    {
+        float jumpDuration = 0.2f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < jumpDuration)
+        {
+            playerRigidbody.AddForce(Vector3.up * (jumpForce / jumpDuration) * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            elapsedTime += Time.fixedDeltaTime;
+            yield return null;
+        }
+
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
